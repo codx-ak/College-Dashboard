13d953d23e41fe62e28924b444f416ef900e0f73
Ak Moorthi <119858092+codx-paradise@users.noreply.github.com>
2023-07-17 10:00:30 +0530

    Database

--- DB/college.sql
+++ DB/college.sql
@@ -1,7 +1,7 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
---
+-- 
 -- Host: 127.0.0.1
 -- Generation Time: Apr 07, 2023 at 08:02 AM
 -- Server version: 10.4.27-MariaDB
